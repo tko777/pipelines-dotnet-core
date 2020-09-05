d7647e8b6a106b7d85820a6ff74647aba14289f6
tko777 <tko777@users.noreply.github.com>
2020-09-05 08:20:14 +0200

    Test

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using pipelines_dotnet_core.Models;
 
+// test
 namespace pipelines_dotnet_core.Controllers
 {
     public class HomeController : Controller
